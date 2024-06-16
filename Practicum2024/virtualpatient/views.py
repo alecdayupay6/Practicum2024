ce17d8650e1e6105288abeb5dd8c80082aba0096
Alec Dayupay <alec.dayupay@obf.ateneo.edu>
2024-06-16 17:37:04 +0800

    Added Permissions

--- Practicum2024/virtualpatient/views.py
+++ Practicum2024/virtualpatient/views.py
@@ -1,42 +1,43 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from .forms import CreateUserForm
+from .decorators import unauthenticated_user, allowed_users
 
 
+# @unauthenticated_user
 def register(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for' + user)
-                return redirect('login')
-        context = {'form': form}
-        return render(request, 'virtualpatient/register.html', context)
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
 
+            group = request.POST.get('group')
+            user.groups.add(Group.objects.get(name=group))
+
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
+    context = {'form': form, 'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/register.html', context)
+
+# @unauthenticated_user
 def login_(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
 
-            user = authenticate(request, username=username, passowrd=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username or Password is incorrect.')
-        context = {}
-        return render(request, 'virtualpatient/login.html', context)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect.')
+    context = {'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/login.html', context)
 
 def logout_(request):
     logout(request)
@@ -44,24 +45,30 @@ def logout_(request):
 
 # @login_required(login_url='login')
 def home(request):
-    return render(request, 'virtualpatient/home.html')
+    context = {'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/home.html', context)
 
 # @login_required(login_url='login')
 def generate(request):
-    return render(request, 'virtualpatient/generate.html')
+    context = {'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/generate.html', context)
 
 # @login_required(login_url='login')
 def select(request):
-    return render(request, 'virtualpatient/select.html')
+    context = {'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/select.html', context)
 
 # @login_required(login_url='login')
 def simulate(request):
-    return render(request, 'virtualpatient/simulate.html')
+    context = {'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/simulate.html', context)
 
 # @login_required(login_url='login')
 def profile(request):
-    return render(request, 'virtualpatient/profile.html')
+    context = {'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/profile.html', context)
 
 # @login_required(login_url='login')
 def faqs(request):
-    return render(request, 'virtualpatient/faqs.html')
+    context = {'is_teacher': request.user.groups.filter(name='teacher').exists()}
+    return render(request, 'virtualpatient/faqs.html', context)
