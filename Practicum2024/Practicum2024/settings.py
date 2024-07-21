0e8ffd79e2dd124263f2eb98cb93b45ec1e8b77c
alecdayupay6 <alec.dayupay@student.ateneo.edu>
2024-07-21 19:34:32 +0800

    Updated Allowed Hosts in settings.py again

--- Practicum2024/Practicum2024/settings.py
+++ Practicum2024/Practicum2024/settings.py
@@ -34,7 +34,7 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://alls-virtual-patient.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
